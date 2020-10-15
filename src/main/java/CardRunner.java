b678488637911b37411e444d798453dd7905548f
kaitlyn grimm <kaitlyngrimm2018@gmail.com>
2020-10-15 17:59:30 +0000

    I finished everything

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,8 +1,21 @@
 public class CardRunner {
     public static void main(String args[]){
         // ☕
-        TerrapinCard card = new TerrapinCard(50);
-        System.out.println(card);
+        TerrapinCard cardBilly = new TerrapinCard(20);
+        TerrapinCard cardBrian = new TerrapinCard(30);
 
+        cardBilly.payGourmet();
+        cardBrian.payEconomical();
+        System.out.println("Billy: " + cardBilly);
+        System.out.println("Brian: " + cardBrian);
+        cardBilly.loadMoney(20);
+        cardBrian.payGourmet();
+        System.out.println("Billy: " + cardBilly);
+        System.out.println("Brian: " + cardBrian);
+        cardBilly.payEconomical();
+        cardBilly.payEconomical();
+        cardBrian.loadMoney(50);
+        System.out.println("Billy: " + cardBilly);
+        System.out.println("Brian: " + cardBrian);
     }
 }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -6,7 +6,33 @@ public class TerrapinCard {
   }
 
   public String toString(){
+    if(this.balance>=150){
+      return "The card has 150.0 dollars";
+    } else{
     return "The card has " + this.balance + " dollars";
+    }
   }
+
+  public void payEconomical(){
+    if(this.balance>= 2.5){
+    this.balance= balance-2.50;
+    }
+  }
+
+  public void payGourmet(){
+    if(this.balance>= 4){
+    this.balance= balance-4;
+    }
+}
+
+public void loadMoney(double amount){
+  if(amount>=0){
+  this.balance= balance+ amount;
+  }
+}
+
+public double getBalance(){
+  return this.balance();
+}
 
 }
